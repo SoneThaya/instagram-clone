d13e3f734860667797d6daeb9bf07bd9291775c5
Sone <sthaya@gmail.com>
2020-07-18 16:51:42 -0700

    created posts component and styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import Post from './Post';
+
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app_headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="logo"
+        />
+      </div>
+
+      <Post />
+      <Post />
+      <Post />
+       
     </div>
   );
 }
