12bd89334d0a0baa6e723176c08bf71c5655c48d
Sone <sthaya@gmail.com>
2020-07-18 21:42:03 -0700

    completed instagram clone, not deployed

--- src/Post.js
+++ src/Post.js
@@ -68,23 +68,26 @@ const Post = ({ user, postId, username, caption, imageUrl }) => {
         }
       </div>
 
-      <form className='post__commentBox'>
-        <input
-          type="text"
-          className='post__input'
-          placeholder='Add a comment...'
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}
-        />
-        <button
-          className="post__button"
-          disabled={!comment}
-          type='submit'
-          onClick={postComment}
-        >
-          Post
-        </button>
-      </form>
+      {user && (
+        <form className='post__commentBox'>
+          <input
+            type="text"
+            className='post__input'
+            placeholder='Add a comment...'
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+          <button
+            className="post__button"
+            disabled={!comment}
+            type='submit'
+            onClick={postComment}
+          >
+            Post
+          </button>
+        </form>
+      )}
+     
     </div>
   )
 }
